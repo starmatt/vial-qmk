437d1ac868e494f32befcfc3f60e4fe9f125d2c3
starmatt <dev+gitlab@starmatt.net>
2024-02-13 15:32:43 +0100

    updated encoders behaviour

--- keyboards/splitkb/elora/keymaps/vial-starmatt/keymap.c
+++ keyboards/splitkb/elora/keymaps/vial-starmatt/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |------|  |------|      |------+------+------+------+------+--------|
  * |  LGUI  |   A  |   S  |   D  |   F  |   G  |      | LAlt |  | RAlt |      |   H  |   J  |   K  |   L  | ;  : |  RGUI  |
  * |--------+------+------+------+------+------+------+------|  |------|------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |Gshift|      |  | Mute | Mouse|   N  |   M  | ,  < | . >  | /  ? | RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |Gshift| Play |  | Mute | Mouse|   N  |   M  | ,  < | . >  | /  ? | RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |CapsLk| LAlt | LCtrl| Space| Num  |  | Sym  | Enter|  Nav | AltGr| Menu |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC  , KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,   KC_5 ,         KC_LSFT,     KC_RSFT,          KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,  KC_0 , KC_ESC ,
       KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,         KC_LCTL,     KC_RCTL,          KC_Y ,  KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
       KC_LGUI , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,         KC_LALT,     KC_RALT,          KC_H ,  KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_RGUI,
-      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , GSHFT  ,XXXXXXX,     KC_MUTE, MOUSE  , KC_N ,  KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , GSHFT  ,KC_MPLY,     KC_MUTE, MOUSE  , KC_N ,  KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
                                 KC_CAPS , KC_LALT, KC_LCTL, KC_SPC ,    NUM,     SYM    , KC_ENT , NAV  , KC_RALT, KC_APP,
 
       KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP,    KC_MUTE,                            KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP,    KC_MUTE
@@ -332,93 +332,93 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     // QWERTY
     [0] = {
         // Left side
+        ENCODER_CCW_CW(KC_PGUP, KC_PGDN),
         ENCODER_CCW_CW(KC_NO, KC_NO),
         ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU),   // Myriad
         // Right side
         ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
         ENCODER_CCW_CW(KC_NO, KC_NO),
         ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)    // Myriad
     },
     // GSHFT
     [1] = {
         // Left side
+        ENCODER_CCW_CW(KC_PGUP, KC_PGDN),
         ENCODER_CCW_CW(KC_NO, KC_NO),
         ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_LEFT, KC_RIGHT),
+        ENCODER_CCW_CW(KC_LEFT, KC_RIGHT),  // Myriad
         // Right side
         ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
         ENCODER_CCW_CW(KC_NO, KC_NO),
         ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)    // Myriad
     },
     // NUM
     [2] = {
         // Left side
+        ENCODER_CCW_CW(KC_PGUP, KC_PGDN),
         ENCODER_CCW_CW(KC_NO, KC_NO),
         ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_LEFT, KC_RIGHT),
+        ENCODER_CCW_CW(KC_LEFT, KC_RIGHT),  // Myriad
         // Right side
         ENCODER_CCW_CW(KC_BRID, KC_BRIU),
         ENCODER_CCW_CW(KC_NO, KC_NO),
         ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)    // Myriad
     },
     // SYM
     [3] = {
         // Left side
+        ENCODER_CCW_CW(KC_UP, KC_DOWN),
         ENCODER_CCW_CW(KC_NO, KC_NO),
         ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_LEFT, KC_RIGHT),
+        ENCODER_CCW_CW(KC_LEFT, KC_RIGHT),  // Myriad
         // Right side
         ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
         ENCODER_CCW_CW(KC_NO, KC_NO),
         ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)    // Myriad
     },
     // NAV
     [4] = {
         // Left side
-        ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_NO, KC_NO),
         ENCODER_CCW_CW(KC_LEFT, KC_RIGHT),
+        ENCODER_CCW_CW(KC_NO, KC_NO),
+        ENCODER_CCW_CW(KC_NO, KC_NO),
+        ENCODER_CCW_CW(KC_LEFT, KC_RIGHT),  // Myriad
         // Right side
         ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
         ENCODER_CCW_CW(KC_NO, KC_NO),
         ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)    // Myriad
     },
     // ADJUST
     [5] = {
         // Left side
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
         ENCODER_CCW_CW(KC_NO, KC_NO),
         ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_LEFT, KC_RIGHT),
+        ENCODER_CCW_CW(KC_LEFT, KC_RIGHT),  // Myriad
         // Right side
         ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
         ENCODER_CCW_CW(KC_NO, KC_NO),
         ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)    // Myriad
     },
     // MOUSE
     [6] = {
         // Left side
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
         ENCODER_CCW_CW(KC_NO, KC_NO),
         ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_LEFT, KC_RIGHT),
+        ENCODER_CCW_CW(KC_LEFT, KC_RIGHT),  // Myriad
         // Right side
         ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
         ENCODER_CCW_CW(KC_NO, KC_NO),
         ENCODER_CCW_CW(KC_NO, KC_NO),
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)    // Myriad
     }
 };
 #endif
